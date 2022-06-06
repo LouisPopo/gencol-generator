4451560b8bec67093edf783c4bd464bef326bd7e
Louis Popovic <louispop@gmail.com>
2022-06-06 11:29:20 -0400

    DualIneqCreateor

--- louis.py
+++ louis.py
@@ -1,18 +1,8 @@
-from ntpath import join
+import argparse
+import shutil
+import string
 from code_juliette.create_data_trip_shifting import create_network_data
 from create_gencol_input import create_gencol_file
-
-
-# nb_instances = 10   # Nombre d'instances 
-# dataset_num = '4b'
-# seed_list = [x for x in range(nb_instances)]
-# max_minute = 25 #1.3 permet d'avoir 1663 trajets et 6 permet d'avoir 385 trajets pour dataset4
-#20 10 7 5 15 3 2 1.8 puis ma
-# str_max = str(max_minute).replace('.', 'p')
-# network_num = dataset_num
-# var_modif = 1 # j'ai utilisé que 1 pour l'instant
-# nb_borne = 4
-# nb_veh = 20
 
 def create_network(nb_instances, dataset_num, max_minute, nb_borne):
 
@@ -45,7 +35,6 @@ def create_network(nb_instances, dataset_num, max_minute, nb_borne):
     create_network_data(seed_list, network_num, lines, max_minute, list_depot, d_time_table, list_recharge, 
         nb_borne, dict_length, dict_length_hlp, dict_lines, var_modif=var_modif)
 
-# fonction qui génère les instances par défaut (sans inégalités duales)
 def create_defaults_instances():
 
     # va lire un fichier .txt et génère les networks ainsi que les fichiers d'inputs gencol correspondants
@@ -69,32 +58,30 @@ def create_defaults_instances():
             default_pb_list_file.write('{}\n'.format(pb))
         
         create_gencol_file(pb_list, nb_veh=nb_veh, dual_variables_file_name='', nb_inequalities=0)
-    # 1. should check if network exists
 
-    # pb_list = [str(network_num)+ "_" + str(x) for x in seed_list]
+def create_duals_ineq_instances():
+    # va chercher les fichiers de variables duales, 
+    # les copies dans le folder network correspondant
+    # run le genereate(avec fichier de dual variables)
+    with open('default_pb_list_file.txt', 'r') as f:
+        default_pb_list = f.read().splitlines()
 
-    # 
-create_defaults_instances()
-
-# On veut creer des inegalites duales
-
-# for i in range(3):
-#     for nb_in in [100,200,300]:
-#         for grp_size in [50,100,200,300]:
-#             if grp_size > nb_in:
-#                 continue
-#             create_gencol_file(pb_list, nb_veh=nb_veh, dual_variables_file_name='dualVarsFirstLinearRelaxProblem4b_0_604_0.out', nb_inequalities=nb_in, grp_size=grp_size, id=i)
-
-# # 1st set of tests
-# # for i in range(3):
-# #     for t in [True, False]:
-# #         for n in [1, 5, 10, 25, 50, 100, 200, 275]:
-# #             create_gencol_file(pb_list, nb_veh=nb_veh, dual_variables_file_name='dualVarsFirstLinearRelaxProblem5a_0_460_0.out', nb_inequalities = n, sequencial_inequalities=t, id=i)
-
-# # 2nd set of tests
+    for pb in default_pb_list:
+        shutil.copy('/MdevspGencolTest/dualVarsFirstLinearRelaxProblem{}.out'.format(pb), 'Networks/Network{}/'.format(pb))
 
 
 
-# for nb_in in [100,200]:
-#     for grp_size in [10,25,50,100]:
-#         create_gencol_file(pb_list, nb_veh=nb_veh, dual_variables_file_name='dualVarsFirstLinearRelaxProblem5a_0_460_0.out', nb_inequalities=nb_in, grp_size=grp_size)
+parser = argparse.ArgumentParser()
+
+parser.add_argument('type', type=str)
+
+args = parser.parse_args()
+
+if args.type == 'default':
+    create_defaults_instances()
+elif args.type == 'dual':
+    create_duals_ineq_instances
+    #print('allp')
+else:
+    print('wrong args')
+
