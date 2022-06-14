117b984e60c62b64de8da4efd335a7cdf7e7a611
Louis Popovic <louispop@gmail.com>
2022-06-14 17:04:44 -0400

    Add logic for default files

--- data_collector.py
+++ data_collector.py
@@ -100,7 +100,12 @@ def folder_aggregation(folder_path):
 
     os.chdir(folder_path)
     for file in glob.glob('reportProblem*'):
-        network, mm, seed, nb_grps, grps_size = file.replace('reportProblem', '').replace('.out', '').split('_')
+
+        if "default" in file:
+            network, mm, seed, grps_size = file.replace('reportProblem', '').replace('.out', '').split('_')
+            nb_grps = 1
+        else:
+            network, mm, seed, nb_grps, grps_size = file.replace('reportProblem', '').replace('.out', '').split('_')
         
         with open(file) as f:
             report = f.read().splitlines()
