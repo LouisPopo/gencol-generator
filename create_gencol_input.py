a4b3a6e5565e5d3205820210746d087b130fcf91
Louis Popovic <louispop@gmail.com>
2022-06-27 14:26:44 -0400

    Remove Max and Count

--- create_gencol_input.py
+++ create_gencol_input.py
@@ -55,7 +55,7 @@ def create_gencol_file(list_pb, fixed_cost=1000, nb_veh=20, sigma_max=363000, sp
 
                 # Pour l'instant on laisse faire les +1000 (cout fixe) et les <0
                 # Finalement on garde le < 0
-                if value < 850 and not take_absolute_value:
+                if value < 850 and not take_absolute_value and "Max" not in dual_variable and "Count" not in dual_variable:
                     dual_variables.append((dual_variable, value))
                 elif value < 850 and take_absolute_value and "Max" not in dual_variable and "Count" not in dual_variable:
                     dual_variables.append((dual_variable, abs(value)))
