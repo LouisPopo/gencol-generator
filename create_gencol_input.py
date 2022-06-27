53413e055d770a6635419c87d37a93fc3a7da86d
Louis Popovic <louispop@gmail.com>
2022-06-27 16:03:00 -0400

    Sort not reverse

--- create_gencol_input.py
+++ create_gencol_input.py
@@ -88,7 +88,8 @@ def create_gencol_file(list_pb, fixed_cost=1000, nb_veh=20, sigma_max=363000, sp
                 if test_new_grp:
 
                     s = random.sample(dual_variables, grp_size)
-                    s.sort(key = lambda pair: pair[1], reverse=True)
+                    # On sort du plus petit au plus grand!
+                    s.sort(key = lambda pair: pair[1])
 
                     min_val = s[0][1]
                     max_val = s[-1][1]
