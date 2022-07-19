ba753f4350cef9b5c17c80588d045057929f6452
Louis Popovic <louispop@gmail.com>
2022-07-19 16:34:55 -0400

    remove validation

--- create_gencol_input.py
+++ create_gencol_input.py
@@ -39,16 +39,16 @@ class IneqGraph:
 
         # add a -> c if no b such that a->b and b-> exists
 
-        # for n in self.graph.neighbors(from_node_name):
+        for n in self.graph.neighbors(from_node_name):
 
-        #     if self.graph.has_edge(from_node_name, n) and self.graph.has_edge(n, to_node_name):
+            if self.graph.has_edge(from_node_name, n) and self.graph.has_edge(n, to_node_name):
 
-        #         # Sauf qu'on augmente quand même les degres
+                # Sauf qu'on augmente quand même les degres
 
-        #         self.graph.add_weighted_edges_from([(from_node_name, '+OutDeg', value)], 'weight', prob=1)
-        #         self.graph.add_weighted_edges_from([('+InDeg', to_node_name, value)], 'weight', prob=1)
+                self.graph.add_weighted_edges_from([(from_node_name, '+OutDeg', value)], 'weight', prob=1)
+                self.graph.add_weighted_edges_from([('+InDeg', to_node_name, value)], 'weight', prob=1)
 
-        #         return
+                return
 
         self.graph.add_weighted_edges_from([(from_node_name, to_node_name, value)], 'weight', prob=prob_right)
 
