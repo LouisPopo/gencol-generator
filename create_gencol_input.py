ef3dec7bac7df5731f82cb1c9ba9df6ee31ec433
Louis Popovic <louispop@gmail.com>
2022-08-12 09:37:33 -0400

    25% new ineq, 0.50*e_ij

--- create_gencol_input.py
+++ create_gencol_input.py
@@ -573,7 +573,7 @@ def create_gencol_file(
                 ###
                 # Adding inequalities with values e_ij
 
-                pair_nb = int(0.75 * len(dual_variables))
+                pair_nb = int(0.25 * len(dual_variables))
                 if pair_nb % 2 != 0:
                     pair_nb += 1
                 
@@ -597,7 +597,7 @@ def create_gencol_file(
                     pi_1_val = dual_variables_vals[pi_1]
                     pi_2_val = dual_variables_vals[pi_2]
 
-                    e_12 = max(0, int(pi_1_val - pi_2_val)) 
+                    e_12 = max(0, 0.50 *  int(pi_1_val - pi_2_val)) 
 
                     tasks_in_new_inequalities.add(pi_1)
                     tasks_in_new_inequalities.add(pi_2)
