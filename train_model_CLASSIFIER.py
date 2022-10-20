1bd1e95d54850bee559de21a402ba5071d74b78a
Louis Popovic <louispop@gmail.com>
2022-10-20 14:53:58 -0400

    LR = 0.005

--- train_model_CLASSIFIER.py
+++ train_model_CLASSIFIER.py
@@ -224,7 +224,7 @@ def train(train_dataloader, val_dataloader, device, model):
 
     loss_fcn = nn.BCELoss()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0)
 
     for epoch in range(400):
         
