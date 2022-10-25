981f97788d4a965b026d889974d371e075d23acf
Louis Popovic <louispop@gmail.com>
2022-10-25 15:07:07 -0400

    Fix max()

--- train_model_CLASSIFIER.py
+++ train_model_CLASSIFIER.py
@@ -180,16 +180,16 @@ class BinaryClassifier(nn.Module):
 
         # v3_5
         nodes_feats, edges_feats = self.egat1(graph, nodes_feats, edges_feats)
-        nodes_feats = nodes_feats.max(1)
-        edges_feats = edges_feats.max(1)
+        nodes_feats = torch.max(nodes_feats, 1).values
+        edges_feats = torch.max(edges_feats, 1).values
 
         nodes_feats, edges_feats = self.egat2(graph, nodes_feats, edges_feats)
-        nodes_feats = nodes_feats.max(1)
-        edges_feats = edges_feats.max(1)
+        nodes_feats = torch.max(nodes_feats, 1).values
+        edges_feats = torch.max(edges_feats, 1).values
 
         nodes_feats, edges_feats = self.egat3(graph, nodes_feats, edges_feats)
-        nodes_feats = nodes_feats.max(1)
-        edges_feats = edges_feats.max(1)
+        nodes_feats = torch.max(nodes_feats, 1).values
+        edges_feats = torch.max(edges_feats, 1).values
 
         h = nodes_feats
 
