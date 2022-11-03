4f49dfc67442aa6c2e0841f92a90a6de504d3861
Louis Popovic <louispop@gmail.com>
2022-11-03 15:49:16 -0500

    Fix models paths

--- train_model_CLASSIFIER.py
+++ train_model_CLASSIFIER.py
@@ -654,9 +654,10 @@ if __name__ == '__main__':
     # Save the model :
     
     dt_string = datetime.now().strftime("%d_%m_%Y__%H_%M_%S")
-    model_folder = 'models/{}'.format(dt_string)
+    model_folder = 'models/{}/'.format(dt_string)
+    model_path = 'models/{}/model.txt'
     os.mkdir(model_folder)
-    torch.save(model.state_dict(), model_folder)
+    torch.save(model.state_dict(), model_path)
 
     print('Testing...')
     test_loss_fnc = nn.BCEWithLogitsLoss()
